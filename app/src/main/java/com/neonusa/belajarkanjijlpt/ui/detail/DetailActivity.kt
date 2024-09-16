edd270f80a24cdc2a4912fdc52d7164d59bd6e54
dickyzuerst <dzuerst@gmail.com>
2024-09-16 18:06:40 +0700

    fix data in kanji subitem

--- app/src/main/java/com/neonusa/belajarkanjijlpt/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/neonusa/belajarkanjijlpt/ui/detail/DetailActivity.kt
@@ -1,12 +1,16 @@
 package com.neonusa.belajarkanjijlpt.ui.detail
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.neonusa.belajarkanjijlpt.R
@@ -28,22 +32,31 @@ class DetailActivity : AppCompatActivity() {
     private val itemsPerPage = 9
 
     private lateinit var kanjiItems: List<KanjiItem>
-    private val kanjiOfTheDayItems = generateDummyKOTD()
-
     private lateinit var kanjiSubitems: List<KanjiSubitem>
+
+//    private val kanjiOfTheDayItems = generateDummyKOTD()
+
     private lateinit var jlptLevel: String
 
     private var jsonString: String? = null
+    private var jsonStringKanjiSubitem: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        loadAds()
+
         jsonString = loadJSONFromAssets("kanji_items.json", this)
         // Parse JSON to list
         val kanjiListType = object : TypeToken<List<KanjiItem>>() {}.type
         kanjiItems = Gson().fromJson(jsonString, kanjiListType)
+
+        jsonStringKanjiSubitem = loadJSONFromAssets("kanji_subitems.json", this)
+        // Parse JSON to list
+        val kanjiSubitemListType = object : TypeToken<List<KanjiSubitem>>() {}.type
+        kanjiSubitems = Gson().fromJson(jsonStringKanjiSubitem, kanjiSubitemListType)
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -59,7 +72,12 @@ class DetailActivity : AppCompatActivity() {
         binding.nextButton.setOnClickListener {
             if ((currentPage + 1) * itemsPerPage < kanjiItems.size) {
                 currentPage++
+
+                val start = currentPage * itemsPerPage
+                val end = minOf(start + itemsPerPage, kanjiItems.size)
+                val pageData = kanjiItems.subList(start, end)
                 loadKanjiData()
+                showSubItems(pageData.first())
             }
         }
 
@@ -90,8 +108,20 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun showSubItems(item: KanjiItem){
-        val kotdAdapter = SubitemAdapter(kanjiOfTheDayItems.filter { it.kanji_item_id == item.id })
+        val kotdAdapter = SubitemAdapter(kanjiSubitems.filter { it.kanji_item_id == item.id })
+
         binding.recyclerviewSubitem.layoutManager = LinearLayoutManager(this)
         binding.recyclerviewSubitem.adapter = kotdAdapter
+
+    }
+
+    private fun loadAds(){
+        val adView = AdView(this)
+        adView.setAdSize(AdSize.BANNER)
+        adView.adUnitId = getResources().getString(R.string.sample_adunit_banner)
+        binding.adviewContainerMain.addView(adView)
+        // Request
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
 }
