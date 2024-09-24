31a72f2d5a02632ea3e3a283cfcb05edc024d884
dickyzuerst <dzuerst@gmail.com>
2024-09-25 03:43:38 +0700

    create new activity kotd

---
+++ app/src/main/java/com/neonusa/belajarkanjijlpt/ui/kanjioftheday/KanjiOfTheDayActivity.kt
@@ -0,0 +1,21 @@
+package com.neonusa.belajarkanjijlpt.ui.kanjioftheday
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.neonusa.belajarkanjijlpt.R
+
+class KanjiOfTheDayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_kanji_of_the_day)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
